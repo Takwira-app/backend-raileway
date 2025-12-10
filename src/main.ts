2266f22be6eaed30b2b52b588341511203e62f52
EmnaMnejja6 <Emnamnejja1808@gmail.com>
2025-12-10 02:35:09 +0100

    added swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
 
+// 👉 Add these imports
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,  { logger: ['error', 'warn', 'log', 'debug'] });
@@ -19,6 +21,19 @@ async function bootstrap() {
     origin: '*', 
     credentials: false,
   });
+
+  // 👉 Add this block for Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Takwira API')
+    .setDescription('API documentation for your football booking app')
+    .setVersion('1.0')
+    .addBearerAuth()  // if you're using JWT Auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0');
